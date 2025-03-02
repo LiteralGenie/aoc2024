30b4441eebcd365b4ba5020cfcb18cc6bbbfe510
nope <nope>
2025-03-02 18:41:42 -0500

    12.2

--- day_12/lib/main.ex
+++ day_12/lib/main.ex
@@ -4,7 +4,7 @@ defmodule Main do
 
     IO.puts([
       "Answer: ",
-      inspect(Solver.score_regions(regions))
+      inspect(Solver.score_regions(regions, fn r -> r.perimeter * r.area end))
     ])
   end
 
@@ -13,7 +13,7 @@ defmodule Main do
 
     IO.puts([
       "Answer: ",
-      inspect(Solver.score_regions(regions))
+      inspect(Solver.score_regions(regions, fn r -> r.perimeter * r.area end))
     ])
   end
 
@@ -22,16 +22,7 @@ defmodule Main do
 
     IO.puts([
       "Answer: ",
-      inspect(Solver.score_regions(regions))
-    ])
-  end
-
-  def ex4() do
-    regions = Solver.solve("./lib/example4")
-
-    IO.puts([
-      "Answer: ",
-      inspect(Solver.score_regions(regions))
+      inspect(Solver.score_regions(regions, fn r -> r.perimeter * r.area end))
     ])
   end
 
@@ -40,40 +31,24 @@ defmodule Main do
 
     IO.puts([
       "Answer: ",
-      inspect(Solver.score_regions(regions))
+      inspect(Solver.score_regions(regions, fn r -> r.perimeter * r.area end))
+    ])
+  end
+
+  def p2() do
+    regions = Solver.solve("./lib/input")
+
+    IO.puts([
+      "Answer: ",
+      inspect(Solver.score_regions(regions, fn r -> r.num_sides * r.area end))
     ])
   end
 end
 
 defmodule Solver do
   defmodule Region do
-    @enforce_keys [:inside, :outside, :value, :area, :perimeter]
-    defstruct [:inside, :outside, :value, :area, :perimeter]
-  end
-
-  defmodule Segment do
-    @enforce_keys [:x, :y, :is_horiz, :num_corners]
-    defstruct [:x, :y, :is_horiz, :num_corners]
-
-    def length(segment) do
-      {a, b} =
-        if segment.is_horiz do
-          segment.x
-        else
-          segment.y
-        end
-
-      length = b - a + 1 + segment.num_corners
-    end
-
-    def has(segment, xy) do
-      {x, y} = xy
-
-      {xmin, xmax} = segment.x
-      {ymin, ymax} = segment.y
-
-      xmin <= x and x <= xmax and ymin <= y and y <= ymax
-    end
+    @enforce_keys [:inside, :outside, :value, :area, :perimeter, :num_sides]
+    defstruct [:inside, :outside, :value, :area, :perimeter, :num_sides]
   end
 
   def solve(fp) do
@@ -94,8 +69,8 @@ defmodule Solver do
             {inside, outside, _} = flood(grid, xy, MapSet.new([xy]))
 
             area = MapSet.size(inside)
-
             perimeter = calc_perimeter(outside, inside)
+            num_sides = count_sides(outside, inside)
 
             regions =
               regions ++
@@ -105,7 +80,8 @@ defmodule Solver do
                     outside: outside,
                     value: Utils.Grid.at(grid, xy).value,
                     area: area,
-                    perimeter: perimeter
+                    perimeter: perimeter,
+                    num_sides: num_sides
                   }
                 ]
 
@@ -188,11 +164,101 @@ defmodule Solver do
     end)
   end
 
-  def score_regions(regions) do
+  defp count_sides(outside, inside) do
+    offsets = [
+      {-1, 0},
+      {1, 0},
+      {0, -1},
+      {0, 1}
+    ]
+
+    # Interior points touching outside
+    border_points =
+      Enum.flat_map(outside, fn {x, y} ->
+        Enum.map(offsets, fn {ox, oy} ->
+          {x + ox, y + oy}
+        end)
+      end)
+      |> Enum.filter(fn xy -> MapSet.member?(inside, xy) end)
+      |> MapSet.new()
+
+    by_x =
+      border_points
+      |> Enum.sort_by(fn {x, y} -> {x, y} end)
+      |> Enum.group_by(fn {x, _} -> x end)
+
+    by_y =
+      border_points
+      |> Enum.sort_by(fn {x, y} -> {y, x} end)
+      |> Enum.group_by(fn {_, y} -> y end)
+
+    by_x |> IO.inspect(label: "by_x")
+    by_y |> IO.inspect(label: "by_y")
+
+    to_check = [
+      # North-facing sides
+      {0, -1},
+      # South
+      {0, 1},
+      # West
+      {-1, 0},
+      # East
+      {1, 0}
+    ]
+
+    counts =
+      to_check
+      |> Enum.map(fn {ox, oy} ->
+        {target_grp, main_getter, minor_getter} =
+          if ox != 0 do
+            {by_x, fn {x, _} -> x end, fn {_, y} -> y end}
+          else
+            {by_y, fn {_, y} -> y end, fn {x, _} -> x end}
+          end
+
+        target_grp
+        |> Map.values()
+        |> Enum.map(fn pts ->
+          # Calculate sides by grouping consecutive points and counting the number of groups
+          chunks =
+            pts
+            # Filter out points whose target side is covered
+            # eg if we're looking for north-facing sides, filter out points that are below another point
+            |> Enum.filter(fn {x, y} -> MapSet.member?(outside, {x + ox, y + oy}) end)
+            |> Enum.chunk_while(
+              {nil, []},
+              fn xy, {prev, chunk} ->
+                coord = minor_getter.(xy)
+
+                if prev == nil or coord - prev == 1 do
+                  {:cont, {coord, chunk ++ [coord]}}
+                else
+                  {:cont, chunk, {coord, [coord]}}
+                end
+              end,
+              fn
+                {prev, []} -> {:cont, {prev, []}}
+                {prev, chunk} -> {:cont, chunk, {prev, []}}
+              end
+            )
+
+          {{ox, oy}, pts, chunks} |> IO.inspect(label: "chekcing")
+
+          length(chunks)
+        end)
+        |> Enum.sum()
+      end)
+
+    counts |> IO.inspect(label: "Side counts")
+
+    Enum.sum(counts)
+  end
+
+  def score_regions(regions, fun) do
     regions
-    |> Enum.sort(fn a, b -> a.value < b.value end)
+    |> Enum.sort_by(fn r -> r.value end)
     |> IO.inspect()
-    |> Enum.map(fn r -> r.perimeter * r.area end)
+    |> Enum.map(fn r -> fun.(r) end)
     |> IO.inspect()
     |> Enum.reduce(0, fn x, acc -> acc + x end)
   end
